0171407627e75cd46b16104d469a9ca40a5df6fa
galanjabal3 <galanjabal3@gmail.com>
2021-06-19 16:04:18 +0700

    test

---
+++ src/main/java/com/example/fuexservice/FuexServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.fuexservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuexServiceApplication.class, args);
+		System.out.println("Hello World!!");
+	}
+
+}
---
+++ src/test/java/com/example/fuexservice/FuexServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fuexservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuexServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
